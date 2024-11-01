ebaa1e0b3cbb0eb30086250119d289feb5337b6e
Jasjaap Singh Dua <jasjaap.s@bayrocklabs.com>
2024-11-01 10:52:04 +0530

    Remove login test case from spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,23 +7,6 @@ context("viewDashboard", () => {
     action.c2cLogin();
   });
 
-  it("Successfully logs in", () => {
-    const cm = new ClientManagement();
-    cm.visit();
-    cm.openAddClientPopup();
-    cm.fillClientName("Test Client");
-    cm.fillClientAddress("Test Address");
-    cm.selectClientCountry("India");
-    cm.selectClientState("Kerala");
-    cm.selectClientCity("Thiruvananthapuram");
-    cm.fillClientZipCode("678901");
-    cm.clickNextButton();
-    cm.fillContractName("Test Contract");
-    cm.selectContractType('PSA')
-    cm.fillContractStartDate(15, 7, 2022);
-    cm.fillContractEndDate(20, 7, 2023);
-    cm.clickNextButton();
-    cm.clickCreateClientButton();
-  })
+  
 })
 
